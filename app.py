012396bfc9cbde6a01c2cde82b7f0fe003261082
theplumblossomsword <121478059+theplumblossomsword@users.noreply.github.com>
2023-01-10 15:28:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://username:token@github.com/username/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://theplumblossomsword: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@github.com/theplumblossomsword/Shouko ok && cd ok && pip3 install -U -r requirements.txt && pip install --upgrade pip && nohup python3 -m InsaneRobot &")
